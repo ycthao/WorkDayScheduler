9ed7aa315204f6243b0141bd8d906e1a1e29eccf
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-01 14:07:30 -0600

    Taking off date trials, adding comments to the moment function

--- scheduler.js
+++ scheduler.js
@@ -1,19 +1,5 @@
-// // Setting a variable to get current date and time
-// let currentDate = new Date();
-
-// // Setting arrays for days and months to print instead of having numbers
-// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-// let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-// // setting variable to get the current date and month out of the array
-// let currentMonth = months[currentDate.getMonth()];
-// let currentDay = days[currentDate.getDay()];
-
-// // printing current date to paragraph tagg with ID name currentDay
-// $("#currentDay").append(currentDay + ", " + currentMonth + " " + currentDate.getDate() + ", " + currentDate.getFullYear());
-
+// Set variable to call date
 let currentDate = moment().format('dddd, MMMM Do, YYYY');
 
+// Print date in the paragraph ID currentDay
 $("#currentDay").append(currentDate);
-
-console.log(moment().format('dddd, MMMM Do, YYYY'));
