934ff7423e3ca6b15dc3b6216ce5ef2cd3820af1
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-01 17:38:13 -0600

    testing color code on a 24 hours

--- scheduler.js
+++ scheduler.js
@@ -10,17 +10,19 @@ $("#currentDay").append(currentDate);
 
 //textarea
 
-let currentHour = moment().format("hA");
+let currentHour = moment().format("H");
 
 
 
 console.log(currentHour);
 
-for(i = 9; i <= 17; i++) {
+for(i = 9; i <= 24; i++) {
 
     // setting variable inside loop
     let hourBlock = $('<div>');
-    let timeSlot = $('<div>');
+    let timeSlot = $('<span>');
+    let eventSlot = $('<textarea>');
+    let btnSave = $('<button>')
 
     // Setting block
     hourBlock.addClass("time-block");
@@ -28,11 +30,26 @@ for(i = 9; i <= 17; i++) {
     $(".container").append(hourBlock);
     
     // Show time
-
     timeSlot.addClass("hour");
     timeSlot.attr("hour-value", i);
     timeSlot.text(i);
     $(".time-block").append(timeSlot);
+
+    // event description
+    eventSlot.addClass("description");
+    eventSlot.attr("hour-value", i);
+    $(".time-block").append(eventSlot);
+
+    // change class color when time has past, or present or future
+    if (i > currentHour) {
+        eventSlot.addClass("future");
+    } else if (i < currentHour) {
+        eventSlot.addClass("past");
+    } else {
+        eventSlot.addClass("present");
+    }
+    eventSlot.text("nothing here yet");
+
 };
 
 
