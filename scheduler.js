2aa46e5479d3847e8c7805ed6958495412eb34b2
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-01 16:20:03 -0600

    Getting the hour and console logging it to make sure it works

--- scheduler.js
+++ scheduler.js
@@ -3,3 +3,22 @@ let currentDate = moment().format('dddd, MMMM Do, YYYY');
 
 // Print date in the paragraph ID currentDay
 $("#currentDay").append(currentDate);
+
+// need textarea to show in div container
+// need save button to shows next to textarea
+// need to color code each textarea
+
+//textarea
+
+let currentHour = moment().format("hA");
+//let setHour = "";
+
+
+console.log(currentHour);
+
+for(i = 9; i <= 17; i++) {
+    console.log(i);
+};
+
+
+
