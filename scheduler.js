c716a3fb840afc6a56c47b785f81d00c760debca
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-01 12:32:04 -0600

    getting the date to show in the console, just need to make it show in the index page

--- scheduler.js
+++ scheduler.js
@@ -1,10 +1,20 @@
 // Setting a variable to get current date and time
 let currentDate = new Date();
 
+// Setting arrays for days and months to print instead of having numbers
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-console.log(months[currentDate.getMonth()]);
+let currentMonth = months[currentDate.getMonth()];
+let currentDay = days[currentDate.getDay()];
+
+console.log(currentDay + ", " + currentMonth + " " + currentDate.getDate());
+
+// console.log(months[currentDate.getMonth()]);
+// console.log(days[currentDate.getDay()]);
+
+// id name where to display date: currentDay
+//$("#currentDay").append("");
 
 // console.log(currentDate.getFullYear());
 // console.log(currentDate.getMonth());
