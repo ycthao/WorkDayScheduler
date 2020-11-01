f024d2bb258c1880409a81e2828779affdb3a407
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-01 12:22:43 -0600

    Getting time, setting an array for days and months

--- scheduler.js
+++ scheduler.js
@@ -1,2 +1,12 @@
-alert("this works!");
-console.log("this works!");
+// Setting a variable to get current date and time
+let currentDate = new Date();
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log(months[currentDate.getMonth()]);
+
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getMonth());
+// console.log(currentDate.getDay());
+// console.log(Date.now());
